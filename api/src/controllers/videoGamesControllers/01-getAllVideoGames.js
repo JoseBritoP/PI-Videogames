ddb38d4d7da8ae23593061b8bc73a3321f420c85
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-04 15:19:40 -0300

    getAllVideogames de la api y la base de datos

--- api/src/controllers/videoGamesControllers/01-getAllVideoGames.js
+++ api/src/controllers/videoGamesControllers/01-getAllVideoGames.js
@@ -4,20 +4,21 @@ const {APIGames, API_KEY} = process.env;
 const cleanArray = require('../../helpers/cleanArray');
 
 const getAllVideogames = async () => {
+  const videogamesBDD = await Videogame.findAll({
+    include:{
+      model: Genre,
+      attributes: ['name'],
+      through: { attributes: []} //Excluye la tabla intermedia;
+    },
+      attributes: {
+        exclude: ['createdAt','updateAt'] // Excluye los campos de Videogame
+    },
+  });
   const videoGamesApiRaw = (await axios.get(`${APIGames}?key=${API_KEY}`)).data.results
   const videoGamesInfo = cleanArray(videoGamesApiRaw);
-  return [...videoGamesInfo]
+  return [...videogamesBDD,...videoGamesInfo]
 };
 
 
 
 module.exports = getAllVideogames;
-/*
-id: {
-name: {
-description: {
-platforms: {
-background_image:
-released: fecha
-rating: 
-        */ 
