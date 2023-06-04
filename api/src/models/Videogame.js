f51d0ebe1cde8b5a2e125eb640db3d5498e60789
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-03 23:30:42 -0300

    models bdd

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,45 @@
+const {DataTypes} = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define("Videogame",
+  {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    background_image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },{ 
+    timestamps: false 
+  });
+};
+
+
+
