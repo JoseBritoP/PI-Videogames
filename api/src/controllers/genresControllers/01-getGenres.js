975a5dca770e6c75019746d5ea3ddcb78a0d058e
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-03 23:29:01 -0300

    Guardando genres en la bdd - funcional bug

--- api/src/controllers/genresControllers/01-getGenres.js
+++ api/src/controllers/genresControllers/01-getGenres.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
 const {APIGenres,API_KEY} =  process.env;
+const {Genre} = require('../../db');
 
 const getGenresVideoGames =  () => {
   const genresApi = axios.get(`${APIGenres}?key=${API_KEY}`)
   // .then((data)=> data.data.results?.map((genre)=>genre.name))
   .then((response)=> {
-    const genres = response.data.results?.map((genre) => ({ name: genre.name }));
-    return genres;
+    const genres = response.data.results;
+    const genresMap = genres?.map((genre) => ({ name: genre.name }));
+    const GenreInBDD = Genre.bulkCreate(genresMap);
+    // Si realizo varias peticiones almaceno los mismos valores una y otra vez
+    //! Arreglar
+    return GenreInBDD;
   })
   .catch((error)=> new Error(error))
   return genresApi;
