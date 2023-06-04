d032dc8754848455d0112cc7d37c6df8401a36ad
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-04 16:49:38 -0300

    getVideogameById funcional

---
+++ api/src/controllers/videoGamesControllers/03-getVideoGameById.js
@@ -0,0 +1,29 @@
+const { Videogame,Genre} = require('../../db');
+const axios = require('axios');
+const {APIGames, API_KEY} = process.env;
+const cleanArrayById = require('../../helpers/cleanArrayById');
+
+const getVideogameByIdApi = async (id) => {
+  //Hacemos l
+  const videoGameApiRaw = (await axios.get(`${APIGames}/${id}?key=${API_KEY}`)).data
+  // console.log(videoGameApiRaw)
+  const videoGameInfo = cleanArrayById([videoGameApiRaw]);
+  // console.log(videoGameInfo)
+  if(!videoGameInfo) throw Error (`No se encontró el videojuego en la API de id ${id}`);
+  return videoGameInfo;
+};
+const getVideogameByIdBDD = async (id) => {
+  const videogameBDD = await findByPk(id,{
+    include:{
+      model: Genre,
+      attributes: ['name'],
+    }
+  });
+  if(!videogameBDD) throw Error (`No se encontró el videojuego en la base de datos de id ${id}`);
+  return videogameBDD;
+};
+
+module.exports = {
+  getVideogameByIdApi,
+  getVideogameByIdBDD,
+}
