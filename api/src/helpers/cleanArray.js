5eceaa80a4a512630dd3f026f7448ae02b2645b4
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-04 15:00:34 -0300

    cleanArray

---
+++ api/src/helpers/cleanArray.js
@@ -0,0 +1,25 @@
+const cleanArray = (array) =>{
+  return array.map((elem)=>{
+    const genresMap = elem.genres?.map((genre) => {
+      return {
+        name : genre.name
+      }});
+    const plataformsMap = elem.platforms?.map((element) => {
+      return {
+        name : element.platform.name
+      }});
+
+    return {
+      id: elem.id,
+      name: elem.name,
+      background_image: elem.background_image,
+      genres: genresMap,
+      platforms: plataformsMap,
+      rating: elem.rating,
+      created: false,
+    }
+  });
+}
+
+
+module.exports = cleanArray;
