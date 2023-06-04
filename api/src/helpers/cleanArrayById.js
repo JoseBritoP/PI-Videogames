467d6450c0d2c42070cbe23d09e6b5afdee7fa5d
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-04 16:50:04 -0300

    cleanArrayById

---
+++ api/src/helpers/cleanArrayById.js
@@ -0,0 +1,28 @@
+const cleanArrayById = (array) =>{
+  return array.map((game)=>{
+    const genresMap = game.genres?.map((genre) => {
+      return { name : genre.name };
+    });
+    const plataformsMap = game.platforms?.map((game) => {
+      return { name : game.platform.name };
+    });
+
+    const descriptionSplit = game.description_raw?.split(".")
+    const description = descriptionSplit.join("")
+
+    return {
+      id: game.id,
+      name: game.name,
+      background_image: game.background_image,
+      genres: genresMap,
+      platforms: plataformsMap,
+      rating: game.rating,
+      released: game.released,
+      description: description,
+      created: false,
+    };
+  });
+};
+
+module.exports = cleanArrayById;
+
