e9b389124a2808d655600798f66e7ae0bc643820
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-06 03:20:25 -0300

    post pending

--- api/src/handlers/videoGamesHandlers.js
+++ api/src/handlers/videoGamesHandlers.js
@@ -1,45 +1,62 @@
 // Controllers:
 
-const getAllVideoGames = require('../controllers/videoGamesControllers/01-getAllVideoGames')
-const getAllVideoGamesByName = require('../controllers/videoGamesControllers/02-getVideoGamesByName')
+// const getAllVideoGames = require('../controllers/videoGamesControllers/01-getAllVideoGames')
+// const getAllVideoGamesByName = require('../controllers/videoGamesControllers/02-getVideoGamesByName')
+const {getAllVideoGames,  getVideoGamesByName, getVideogameByIdApi,getVideogameByIdBDD,createVideoGame, deleteAVideoGameAPI,deleteAVideoGameBDD } =  require('../controllers/index');
 // Handlers:
 
 const getVideoGames = async (req,res) =>{
-  const {name} = req.query;
+  const { name } = req.query;
+  const { page } = req.query
   try {
-    if(!name){
-      const videogames = await getAllVideoGames()
-      return res.status(200).json(videogames)
-      // return res.status(200).json({message: `En esta ruta se traerán todos los videojuegos`})
-      
-    }else{
-      const videogamesName = await getAllVideoGamesByName(name);
-      return res.status(200).json(videogamesName)
-      // return res.status(200).json({message: `En esta ruta se traerán todos los videojuegos de nombre: ${name}`})
-    }
+      const videogames = name ? await getVideoGamesByName(name) : await getAllVideoGames(page);
+      return res.status(200).json(videogames);
   } catch (error) {
     return res.status(404).json({error: error.message});
+  };
+};
+
+const getVideoGame = async (req,res)=>{
+  const {id} = req.params;
+  // return res.status(200).json({message:`Aquí se traerá el videojuego de id: ${id}`});
+  try {
+    const videogameFound = isNaN(id) ? await getVideogameByIdBDD(id) : await getVideogameByIdApi(id);
+    return res.status(200).json(videogameFound);
+  } catch (error) {
+    return res.status(404).json({error: error.message})
+  }
+}
+
+const postVideoGame = async (req,res) => {
+  const {name,description,plataforms,image,released,rating,genres} = req.body;
+  try {
+    const newVideoGame = await createVideoGame(name,description,plataforms,image,released,rating,genres);
+    if(!newVideoGame) throw Error (`No se pudo crear el videojuego`);
+    return res.status(201).json({created: newVideoGame});
+  } catch (error) {
+    return res.status(404).json({error:error.message});
+  }
+  // return res.status(200).json({message: `En esta ruta se creará un videojuego con los siguientes datos:
+  //  nombre: ${name}
+  //  descripción: ${description}
+  //  plataformas: ${plataforms}
+  //  imagen: ${image}
+  //  fecha: ${date}
+  //  rating: ${rating}   
+  //  `})
+};
+
+const deleteVideoGame = async (req,res) => {
+  const { id } = req.params;
+  try {                                // bdd : api
+    const videogames = isNaN(id) ? await deleteAVideoGameBDD(id) : await deleteAVideoGameAPI(id)
+    return res.status(200).json(videogames)
+  } catch (error) {
+    return res.status(404).json({error:error.message});
   }
 };
 
-const getVideoGame = (req,res)=>{
-  const {id} = req.params;
-  return res.status(200).json({message:`Aquí se traerá el videojuego de id: ${id}`});
-}
-
-const postVideoGame = (req,res) => {
-  const {name,description,plataforms,image,date,rating} = req.body;
-  return res.status(200).json({message: `En esta ruta se creará un videojuego con los siguientes datos:
-   nombre: ${name}
-   descripción: ${description}
-   plataformas: ${plataforms}
-   imagen: ${image}
-   fecha: ${date}
-   rating: ${rating}   
-   `})
-}
-
 
 module.exports = {
-  getVideoGames,getVideoGame,postVideoGame
+  getVideoGames,getVideoGame,postVideoGame, deleteVideoGame,
 }
