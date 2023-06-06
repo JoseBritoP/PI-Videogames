7765cbde9f9c9de6c18ff2b28691a07b7e9eff36
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-05 23:09:10 -0300

    obtención y almacenamiento de genres funcional

--- api/src/controllers/genresControllers/01-getGenres.js
+++ api/src/controllers/genresControllers/01-getGenres.js
@@ -2,20 +2,33 @@ const axios = require('axios');
 const {APIGenres,API_KEY} =  process.env;
 const {Genre} = require('../../db');
 
-const getGenresVideoGames =  () => {
-  const genresApi = axios.get(`${APIGenres}?key=${API_KEY}`)
-  // .then((data)=> data.data.results?.map((genre)=>genre.name))
-  .then((response)=> {
-    const genres = response.data.results;
-    const genresMap = genres?.map((genre) => ({ name: genre.name }));
-    const GenreInBDD = Genre.bulkCreate(genresMap);
-    // Si realizo varias peticiones almaceno los mismos valores una y otra vez
-    //! Arreglar
-    return GenreInBDD;
-  })
-  .catch((error)=> new Error(error))
-  return genresApi;
-}
+const getGenresVideoGames = async () => {
+  try {
+    // Guardo el valor del objeto axios en response
+    const response = await axios.get(`${APIGenres}?key=${API_KEY}`);
 
+    // Guardo en genres el array de objetos que nos interesa, genres
+    const genres = response.data.results;
+
+    // Como es un array, realizo un map de toda la info solo me quedó con la propiedad nombre
+    const genresMap = genres?.map((genre) => ({ name: genre.name }));
+
+    // En promises hago un map de genresMap donde por cada género que me da la api, busco o creo un objeto donde tenga como nombre el género
+    const promises = genresMap.map((genre) => Genre.findOrCreate({ where: genre }));
+    
+    // Utilizo Promise.all para resolver la promesa del método findOrCreate
+    // Guardo en results el valor de resolución de la promesa que hay en promises
+    // genre es {name: genre.name} por parte de genresMap
+    const results = await Promise.all(promises);
+  
+    // Guardo en createdGenres un mapeo de los resultados trayendome solo el primer valor, siendo este el objeto creado o encontrado
+    // utilizo .get({plain:true}) para solo mostrar en consola dataValues { id, name }
+    const createdGenres = results.map((result) => result[0].get({ plain: true }));
+    // console.log(createdGenres);
+    return createdGenres;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 module.exports = getGenresVideoGames;
