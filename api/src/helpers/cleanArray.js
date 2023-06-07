4114d7c2f21be1693f86e56dd147a4d2653430c6
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-07 15:00:24 -0300

    funcional

--- api/src/helpers/cleanArray.js
+++ api/src/helpers/cleanArray.js
@@ -4,19 +4,11 @@ const cleanArray = (array) =>{
       return {
         name : genre.name
       }});
-    const plataformsMap = elem.platforms?.map((element) => {
-      return {
-        name : element.platform.name
-      }});
-
     return {
       id: elem.id,
       name: elem.name,
       background_image: elem.background_image,
       genres: genresMap,
-      platforms: plataformsMap,
-      rating: elem.rating,
-      released: elem.released,
       created: false,
     }
   });
