8aa66295bc7ef9598c06787985146f9acf3fbc5d
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-08 04:05:19 -0300

    post and delete handler

--- api/src/handlers/genresHandler.js
+++ api/src/handlers/genresHandler.js
@@ -1,6 +1,8 @@
 // Controller:
 
-const getGenresVideoGames = require('../controllers/genresControllers/01-getGenres')
+const getGenresVideoGames = require('../controllers/genresControllers/01-getGenres');
+const createGenre = require('../controllers/genresControllers/02-createGenre');
+const deleteGenreController = require('../controllers/genresControllers/03-deleteGenre');
 
 // Handler:
 
@@ -14,5 +16,29 @@ const getAllGenres = async (req,res)=>{
   // return res.status(200).json({message: `En esta ruta se traerán los géneros de los videojuegos`})
 }
 
+const postGenre = async (req,res) => {
+  const {name} = req.body;
+  try {
+    const newGenre = await createGenre(name);
+    return res.status(201).json({genreCreated: newGenre});
+  } catch (error) {
+    return res.status(409).json({error: error.message})
+  }
+  // return res.status(201).json({message: `Se creará un nuevo genero de nombre: ${name}`})
+};
 
-module.exports = getAllGenres
+const deleteGenre = async (req,res) => {
+  const { id } = req.params;
+  try {
+    const genreDeleted = await deleteGenreController(id);
+    return res.status(200).json({deleted: genreDeleted})
+  } catch (error) {
+    return res.status(400).json({error: error.message})
+  }
+};
+
+module.exports = {
+  getAllGenres,
+  postGenre,
+  deleteGenre,
+}
