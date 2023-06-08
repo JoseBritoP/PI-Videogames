39edb33d53326283d8e0e1c5b195195c0d067812
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-08 04:04:39 -0300

    agregando post y delete

--- api/src/routes/genresRoutes.js
+++ api/src/routes/genresRoutes.js
@@ -1,13 +1,15 @@
 const {Router} = require('express')
 
 // Handler:
-const getAllGenres = require('../handlers/genresHandler')
+const {getAllGenres, postGenre, deleteGenre} = require('../handlers/genresHandler')
 // Router:
 
 const genresRouter = Router();
 
 // Enrutado:
 
-genresRouter.get('/',getAllGenres)
+genresRouter.get('/',getAllGenres);
+genresRouter.post('/',postGenre);
+genresRouter.delete('/:id',deleteGenre)
 
 module.exports = genresRouter;
