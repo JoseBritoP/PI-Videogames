9bdf1690f083ae5d6d7eb19929e3a2f4b760cb68
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-08 04:04:20 -0300

    solucionado la necesida de un json.parse

--- api/src/helpers/cleanArrayDetailBDD.js
+++ api/src/helpers/cleanArrayDetailBDD.js
@@ -1,12 +1,11 @@
 const cleanArrayDetailBDD = (videoGame) => {
-  // console.log(videoGame.platforms)
+  // console.log(videoGame.platforms) // ["PC","PS5","XBOX-ONE"]
   return {
     id: videoGame.id,
     name: videoGame.name,
     background_image: videoGame.background_image,
     description: videoGame.description,
-    // platforms: videoGame.platforms?.map((platform)=>({name: platform})),
-    platforms: videoGame.platforms.map((platform) => JSON.parse(platform)),
+    platforms: videoGame.platforms.map((platform) =>({nane: platform})),
     genres: videoGame.Genres.map((genre) => ({ name: genre.name })),
     rating: videoGame.rating,
     released: videoGame.released,
