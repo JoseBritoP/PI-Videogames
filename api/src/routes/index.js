99dded7961a1d2f6ea0b2c05de03be4eabd3a047
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-10 15:34:31 -0300

    index routes clean

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,13 @@ const {Router} = require('express');
 // Importamos las routers:
 const videogamesRouter = require('./videogamesRoutes')
 const genresRouter = require('./genresRoutes')
+const platformsRouter = require('./platforms')
 const router = Router();
 
 // Conexión al enrutado:
 
 router.use('/videogames',videogamesRouter)
 router.use('/genres',genresRouter)
+router.use('/platforms',platformsRouter)
+
 module.exports = router;
