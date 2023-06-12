0794911ef6c705bf07f15531cc7a743f8c62ec0a
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-12 14:11:42 -0300

    landing

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import style from './Landing.module.css'
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const handleStart = (e) => {
+    e.preventDefault();
+    navigate("/home");
+  };
   return (
-    <div>Desde landing</div>
+    <div className={style['landing-container']}>
+      <div className={style["landing-p"]}>
+        <p>Welcome to our gaming paradise! Explore a world of epic adventures, immersive gameplay, and endless fun. </p>
+        <p>Discover a vast collection of games, search by genre and rating, and find the perfect match for your gaming preferences. </p>
+        <p>Dive into detailed game profiles, unleash your creativity as a game creator, and connect with fellow gamers in our vibrant community. Level up your gaming adventure today!</p>
+      </div>
+      <Link>
+        <input type="button" className={style['button']} value="Enjoy!"onClick={(e)=>handleStart(e)}/>
+      </Link>
+    </div>
   )
 }
 
