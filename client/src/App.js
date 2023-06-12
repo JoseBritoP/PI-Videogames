4e0b1f339cf8a60af95925de2394242ebf7b2bff
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-11 21:24:49 -0300

    clean

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import  Navbar  from './components/Navbar/Navbar'
+import { Home, Detail, Form, Error, Landing } from './views/index';
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <div>
+      {location.pathname !== "/" && <Navbar />}
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/create" element={<Form />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </div>
     </div>
   );
 }
