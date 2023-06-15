b70a54eb3dc623840ff96b69d4573c88272dfe79
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-15 20:12:30 -0300

    navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 import style from './Navbar.module.css'
 import searchIcon from '../../image/icons/search.png'
+import { getVideogamesBySearch} from '../../redux/actions'
+import { useDispatch } from 'react-redux'
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  
+  const [inputValue, setInputValue] = useState("");
+  const [hasSearch, setHasSearch] = useState(false);
+
+  const handleSearchSubmit = () => {
+    setHasSearch(true);
+    handleSearchRequest();
+  };
+
+  const handleSearchInput = (event) => {
+    const query = event.target.value;
+    setInputValue(query);
+  };
+
+  const handleClearSearch = () => {
+    if (inputValue === "" || inputValue.trim() === "") {
+      return
+    };
+    setInputValue("");
+    setHasSearch(false);
+    dispatch(getVideogamesBySearch([]));
+  };
+
+  const handleSearchRequest = () => {
+    if (inputValue.length <= 4) return;
+    if (/\d/.test(inputValue)) return;
+    if (inputValue === "" || inputValue.trim() === "") return;
+    dispatch(getVideogamesBySearch(inputValue));
+  };
+
+  const searchDisabled = inputValue === "" || inputValue.trim() === "" || /\d/.test(inputValue) || inputValue.length <= 4 || !/^[A-Za-z0-9:]+$/.test(inputValue); 
+
   return (
     <nav>
       <div className={style['nav-container']}>
@@ -12,15 +47,16 @@ const Navbar = () => {
         </div>
         <div className={style['nav-search']}>
           <img src={searchIcon} alt='search'/>
-          <input type="text" name="search" id="" placeholder='Search a videogame...'/>
+          <input type="text" value={inputValue} placeholder="Search..." onChange={handleSearchInput} />
+        <button onClick={handleSearchSubmit} disabled={searchDisabled}>Search</button>
+        {hasSearch && <button onClick={handleClearSearch}>Clear</button>}
         </div>
         <div className={style['nav-routes']}>
-          <Link to={"/home"}><h3>Home</h3></Link>
-          <Link to="/create"><h3>Create a videogame!</h3></Link>
-          <Link to="/"><h3>Log out</h3></Link>
+          <Link to={"/home"} style={{textDecoration:"none"}}><h3>Home</h3></Link>
+          <Link to="/create" style={{textDecoration:"none"}}><h3>Create a videogame!</h3></Link>
+          <Link to="/" style={{textDecoration:"none"}}><h3>Log out</h3></Link>
         </div>
       </div>
-        {/* <SearchBar onSearch={onSearch}/> */}
     </nav>
   )
 }
