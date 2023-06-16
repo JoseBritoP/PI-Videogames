31cdd7cf7fa18b6d7869db889a50e735379bd838
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-16 15:12:56 -0300

    Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,43 +1,13 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import style from './Navbar.module.css'
-import searchIcon from '../../image/icons/search.png'
-import { getVideogamesBySearch} from '../../redux/actions'
-import { useDispatch } from 'react-redux'
+// import searchIcon from '../../image/icons/search.png'
+// import { getVideogamesBySearch} from '../../redux/actions'
+// import { useDispatch } from 'react-redux'
+import SearchBarComponent from '../SearchBarComponent/SearchBarComponent'
+const Navbar = (props) => {
 
-const Navbar = () => {
-  const dispatch = useDispatch();
-  
-  const [inputValue, setInputValue] = useState("");
-  const [hasSearch, setHasSearch] = useState(false);
-
-  const handleSearchSubmit = () => {
-    setHasSearch(true);
-    handleSearchRequest();
-  };
-
-  const handleSearchInput = (event) => {
-    const query = event.target.value;
-    setInputValue(query);
-  };
-
-  const handleClearSearch = () => {
-    if (inputValue === "" || inputValue.trim() === "") {
-      return
-    };
-    setInputValue("");
-    setHasSearch(false);
-    dispatch(getVideogamesBySearch([]));
-  };
-
-  const handleSearchRequest = () => {
-    if (inputValue.length <= 4) return;
-    if (/\d/.test(inputValue)) return;
-    if (inputValue === "" || inputValue.trim() === "") return;
-    dispatch(getVideogamesBySearch(inputValue));
-  };
-
-  const searchDisabled = inputValue === "" || inputValue.trim() === "" || /\d/.test(inputValue) || inputValue.length <= 4 || !/^[A-Za-z0-9:]+$/.test(inputValue); 
+  const {resetPagination} = props
 
   return (
     <nav>
@@ -45,12 +15,9 @@ const Navbar = () => {
         <div className={style['nav-image']}>
         <h1>Logo videogames</h1>
         </div>
-        <div className={style['nav-search']}>
-          <img src={searchIcon} alt='search'/>
-          <input type="text" value={inputValue} placeholder="Search..." onChange={handleSearchInput} />
-        <button onClick={handleSearchSubmit} disabled={searchDisabled}>Search</button>
-        {hasSearch && <button onClick={handleClearSearch}>Clear</button>}
-        </div>
+
+        <SearchBarComponent resetPagination={resetPagination}/>
+        
         <div className={style['nav-routes']}>
           <Link to={"/home"} style={{textDecoration:"none"}}><h3>Home</h3></Link>
           <Link to="/create" style={{textDecoration:"none"}}><h3>Create a videogame!</h3></Link>
