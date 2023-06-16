10f60edf6bc7333e9542a57ec1c48f062ef3aee0
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-16 15:19:23 -0300

    Cards

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import Card from '../Card/Card'
 import style from './Cards.module.css'
+import Spinner from '../Spinner/Spinner'
 
 const Cards = (props) => {
 
 	const {videogames} = props;
+
 	return (
     <div className={style['card-grid']}>
-			{videogames?.map((videogame)=>{
-				return <Card
-					key={videogame.id}
-					id={videogame.id}
-					name={videogame.name}
-					image={videogame.background_image}
-					genres={videogame.genres.map((genre)=> genre.name + " ")}
-					platforms= {videogame.platforms.map((platform)=>platform.name + " ")}
-					released = {videogame.released}
-					rating = {videogame.rating}
-				/>
-			})}
+			{videogames.length === 0 ? (<Spinner/>) 
+				:(
+					videogames.map((videogame) => (
+						<Card
+							key={videogame.id}
+							id={videogame.id}
+							name={videogame.name}
+							image={videogame.background_image}
+							genres={videogame.genres.map((genre) => genre.name + " ")}
+							platforms={videogame.platforms.map((platform) => platform.name + " ")}
+							released={videogame.released}
+							rating={videogame.rating}
+						/>))
+				)
+			}
 		</div>
   )
 }
