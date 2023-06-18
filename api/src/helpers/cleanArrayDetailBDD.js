bb9487dae6d017166060cba28115fc7e0ff77808
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-18 12:23:06 -0300

    cleanArray

--- api/src/helpers/cleanArrayDetailBDD.js
+++ api/src/helpers/cleanArrayDetailBDD.js
@@ -5,7 +5,7 @@ const cleanArrayDetailBDD = (videoGame) => {
     name: videoGame.name,
     background_image: videoGame.background_image,
     description: videoGame.description,
-    platforms: videoGame.platforms.map((platform) =>({nane: platform})),
+    platforms: videoGame.platforms.map((platform) =>({name: platform})),
     genres: videoGame.Genres.map((genre) => ({ name: genre.name })),
     rating: videoGame.rating,
     released: videoGame.released,
