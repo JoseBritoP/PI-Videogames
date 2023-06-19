6057f442508b30771375e656dec6ea86eb61d930
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-18 21:46:08 -0300

    App

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import  Navbar  from './components/Navbar/Navbar'
 import { Home, Detail, Form, Error, Landing } from './views/index';
@@ -6,12 +7,18 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 function App() {
   const location = useLocation();
 
+  const [currentPage,setCurrentPage] = useState(1)
+  
+  const resetPagination = () => {
+    setCurrentPage(1)
+  };
+
   return (
     <div className="App">
-      {location.pathname !== "/" && <Navbar />}
+      {location.pathname !== "/" && <Navbar resetPagination={resetPagination}/>}
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<Home currentPage={currentPage} setCurrentPage={setCurrentPage}/>} />
           <Route path="/create" element={<Form />} />
           <Route path="/detail/:id" element={<Detail />} />
           <Route path="/*" element={<Error />} />
