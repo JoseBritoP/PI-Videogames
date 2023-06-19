b7b0f05041b9af01e0bfba252b73425d41b050e0
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-18 21:43:26 -0300

    pagination component

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import style from './Pagination.module.css'
 const Pagination = (props) => {
   
-  const {currentPage,setCurrentPage,totalPages,pageNumbers} = props;
+  const {currentPage,setCurrentPage,totalPages,/*pageNumbers*/} = props;
+  
+  //* Limitar la cantidad de botones
+  const max_Buttons = 5;
+  const startPage = Math.max(1,currentPage - Math.floor(max_Buttons/2));
+  const endPage = Math.min(startPage + max_Buttons - 1, totalPages)
 
   const nextPage = () => {
     const nextPage = currentPage + 1;
@@ -25,17 +30,29 @@ const Pagination = (props) => {
 
   return (
     <div className={style['pagination-container']}>
-      <button onClick={goToFirstPage} disabled={currentPage === 1}> {"<<"}</button>
-      <button onClick={prevPage} disabled={currentPage === 1}>{"<"}</button>
-        {pageNumbers.map((pageNumber) => (
-          <button key={pageNumber} onClick={() => setCurrentPage(pageNumber)} className={currentPage === pageNumber ? style['active'] : ''}>
-            {pageNumber}
-          </button>
-        ))}
-      <button onClick={nextPage} disabled={currentPage === totalPages}>{">"}</button>
-      <button onClick={goToLastPage} disabled={currentPage === totalPages}> {">>"}</button>
+      <button onClick={goToFirstPage} disabled={currentPage === 1}>
+        {"<<"}
+      </button>
+      <button onClick={prevPage} disabled={currentPage === 1}>
+        {"<"}
+      </button>
+      {Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index).map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => setCurrentPage(pageNumber)}
+          className={currentPage === pageNumber ? style['active'] : ''}
+        >
+          {pageNumber}
+        </button>
+      ))}
+      <button onClick={nextPage} disabled={currentPage === totalPages}>
+        {">"}
+      </button>
+      <button onClick={goToLastPage} disabled={currentPage === totalPages}>
+        {">>"}
+      </button>
     </div>
-  )
+  );
 }
 
 export default Pagination
