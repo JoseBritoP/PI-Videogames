5fd98f5f696376d4d8532d4e7c018237da90cbdf
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-21 16:33:38 -0300

    Model update

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -2,19 +2,20 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("Genre", 
-  {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-  },
-  { 
-    timestamps: false 
-  });
+    { 
+      timestamps: false 
+    }
+  );
 };
