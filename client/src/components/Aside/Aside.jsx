e01c49d0a8832f086acfe87995efb8ddae0ea9ea
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-22 02:12:31 -0300

    Aside

--- client/src/components/Aside/Aside.jsx
+++ client/src/components/Aside/Aside.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import style from './Aside.module.css'
-const Aside = () => {
+import style from './Aside.module.css';
+import {AsideFilter,AsideOrder} from '../AsideSubComponents/index';
+
+const Aside = (props) => {
+  const {resetPagination} = props;
+
   return (
     <div className={style['aside-container']}>
-      <div className={style['aside-order']}>
-        <h1>Order By:</h1>
-        <p>ASC</p>
-        <p>DESC</p>
-      </div>
-      <div className={style['aside-filtered']}>
-        <h1>Filtered by:</h1>
-        <p>Genres</p>
-        <p>Rating</p>
-        <p>Released</p>
+      <div className={style['fixed']}>
+
+        <AsideOrder/>
+
+        <AsideFilter resetPagination={resetPagination}/>
+
+        {/* <AsideOrigin/> */}
+        <div className={style['message']}>
+        <p>WARNING! <br/> <span>ORIGIN: 
+          <br/>The values: {"(Both - InApi - InBdd)"} is used for a more specific search after filtering by genre.<br/>
+          And the values: {"(Clear API CREATED)"} is where filters and sorting will be applied.<br/>
+          ENJOY!</span></p>
+        </div>
       </div>
     </div>
   )
