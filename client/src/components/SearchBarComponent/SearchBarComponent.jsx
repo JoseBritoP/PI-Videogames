41a7ccceef89db92756b1df64cf561dcf4c74212
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-21 21:21:15 -0300

    Searchbar

--- client/src/components/SearchBarComponent/SearchBarComponent.jsx
+++ client/src/components/SearchBarComponent/SearchBarComponent.jsx
@@ -1,35 +1,39 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState} from 'react'
 import searchIcon from '../../image/icons/search.png'
-import { getVideogamesBySearch, clearVideogamesBySearch} from '../../redux/actions'
+import { getVideogamesBySearch, clearVideogames, getAllVideogames} from '../../redux/actions'
 import { useDispatch } from 'react-redux'
-import style from './SearchBarComponent.module.css'
-import { useNavigate } from 'react-router-dom'
-const SearchBarComponent = (props) => {
+import style from './SearchBarComponent.module.css';
+import { useNavigate } from 'react-router-dom';
 
+const SearchBarComponent = (props) => {
   const navigate = useNavigate()
   const {resetPagination} = props
 
   const dispatch = useDispatch()
   
   const [inputValue, setInputValue] = useState("");
-  const [hasSearch, setHasSearch] = useState(false);
+  const [hasSearch, setHasSearch] = useState(false); // Si se hizo una busqueda o no
 
-  const handleSearchSubmit = () => {
-    setHasSearch(true);
-    handleSearchRequest();
-    navigate('/home')
-  };
 
   const handleSearchInput = (event) => {
     const query = event.target.value;
     setInputValue(query);
   };
   
+  const handleSearchSubmit = () => {
+    setHasSearch(true);
+    resetPagination();
+    handleSearchRequest();
+    navigate('/home');
+  };
+
   const handleSearchRequest = () => {
     if (inputValue.length < 4) return;
     if (/\d/.test(inputValue)) return;
     if (inputValue === "" || inputValue.trim() === "") return;
+    resetPagination();
+    dispatch(clearVideogames());
     dispatch(getVideogamesBySearch(inputValue.trim()));
   };
   
@@ -37,8 +41,10 @@ const SearchBarComponent = (props) => {
     if (inputValue === "" || inputValue.trim() === "") return;
     setInputValue("");
     setHasSearch(false);
-    resetPagination()
-    dispatch(clearVideogamesBySearch());
+    resetPagination();
+    navigate('/home');
+    dispatch(clearVideogames());
+    dispatch(getAllVideogames());
   };
   
   const searchDisabled = inputValue === "" || inputValue.trim() === "" || /\d/.test(inputValue) || inputValue.length < 3 || !/^[a-zA-Z0-9\s\-:.]+$/u.test(inputValue);
@@ -46,7 +52,7 @@ const SearchBarComponent = (props) => {
   return (
     <div className={style['searchbar-container']}>
       <img src={searchIcon} alt='search'/>
-      <input type="text" value={inputValue} placeholder="Search..." onChange={handleSearchInput} />
+      <input type="text" value={inputValue} placeholder="God of War, Tomb Raider..." onChange={handleSearchInput} />
       <button onClick={handleSearchSubmit} disabled={searchDisabled}>Search</button>
         {hasSearch && <button onClick={handleClearSearch}>Clear</button>}
     </div>
