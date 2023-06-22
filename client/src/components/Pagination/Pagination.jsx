e0cffeb1daa6e14a46a4dc3527805841f64a99d5
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-22 12:54:57 -0300

    pagination

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -7,7 +7,19 @@ const Pagination = (props) => {
   //* Limitar la cantidad de botones
   const max_Buttons = 5;
   const startPage = Math.max(1,currentPage - Math.floor(max_Buttons/2));
+  //currentPage  = 6
+  // Queremos mostrar 5 botones por páginación
+  // Math.floor(5/2) -> 2
+  // Math.max(1, 6 - 2);
+  // Math.max(1,4) -> 4
+  // startPage = 4
+ 
   const endPage = Math.min(startPage + max_Buttons - 1, totalPages)
+
+  // Math.min(4 + 5 - 1, 7);
+  // (4+5-1, 7)
+  // 4+4,7 
+  // 7
 
   const nextPage = () => {
     const nextPage = currentPage + 1;
@@ -28,6 +40,14 @@ const Pagination = (props) => {
     setCurrentPage(1);
   };
 
+  const pages =[];
+  for (let pageNumber = startPage; pageNumber <= endPage; pageNumber++) {
+    pages.push(
+      <button key={pageNumber} onClick={() => setCurrentPage(pageNumber)} className={currentPage === pageNumber ? style['active'] : ''}> 
+        {pageNumber}
+      </button>
+    );
+  }
   return (
     <div className={style['pagination-container']}>
       <button onClick={goToFirstPage} disabled={currentPage === 1}>
@@ -36,11 +56,7 @@ const Pagination = (props) => {
       <button onClick={prevPage} disabled={currentPage === 1}>
         {"<"}
       </button>
-      {Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index).map((pageNumber) => (
-        <button key={pageNumber} onClick={() => setCurrentPage(pageNumber)} className={currentPage === pageNumber ? style['active'] : ''}>
-          {pageNumber}
-        </button>
-      ))}
+      {pages}
       <button onClick={nextPage} disabled={currentPage === totalPages}>
         {">"}
       </button>
