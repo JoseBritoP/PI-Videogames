0ef1c559974c158b330dc9ffd0a8ed50ddc947a2
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-21 21:21:59 -0300

    Error

--- client/src/views/Error/Error.jsx
+++ client/src/views/Error/Error.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styles from './Error.module.css'
-
+import { getAllVideogames } from '../../redux/actions'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 const Error = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const backToHome = (event) =>{
+    event.preventDefault();
+    navigate('/home');
+    dispatch(getAllVideogames());
+  }
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Error 404: Página no encontrada</h1>
       <div className={styles['text-container']}>
        <p className={styles.text}> Lo sentimos, la página que estás buscando no existe.</p>
-       <p className={styles.link}><Link to="/home" style={{textDecoration:"none"}}>Volver a Home</Link></p>
+       <button onClick={(e)=>{backToHome(e)}}> Home </button>
       </div>
     </div>
   )
