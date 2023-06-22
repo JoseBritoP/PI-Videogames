1411f4763d274fb820cc82a7562b9e180d510a94
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-22 02:20:03 -0300

    StarRating

--- client/src/components/StarRatingForm/StarRatingForm.jsx
+++ client/src/components/StarRatingForm/StarRatingForm.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
 import star from '../../image/icons/star.png';
 import starEmpty from '../../image/icons/starEmpty.png';
-// import starlight from '../../image/icons/starlight.png';
 import style from './StarRatingForm.module.css'; // Importa el archivo CSS para el componente
 
-const StarRatingForm = ({rating, setRating, handleChangeForm}) => {
- 
-  const [clickedStar, setClickedStar] = useState(null);
+const StarRatingForm = ({ rating, setRating, handleChangeForm }) => {
+  const [clickedStar, setClickedStar] = useState(rating);
 
   const handleStarClick = (selectedRating) => {
-    setRating(selectedRating);
-    setClickedStar(selectedRating);
-    setTimeout(() => {
-      setClickedStar(null);
-    }, 1000);
-  
-    // Llama a la función handleChangeForm con el evento
-    handleChangeForm({ target: { name: 'rating', value: selectedRating, type: 'number' } });
+    const newRating = selectedRating === rating ? 0 : selectedRating;
+    setRating(newRating);
+    setClickedStar(newRating);
+    handleChangeForm({
+      target: { name: 'rating', value: newRating, type: 'number' },
+    });
   };
 
   return (
@@ -26,10 +22,12 @@ const StarRatingForm = ({rating, setRating, handleChangeForm}) => {
         {[1, 2, 3, 4, 5].map((value) => (
           <img
             key={value}
-            src={value <= rating ? star : starEmpty}
+            src={value <= clickedStar ? star : starEmpty}
             alt={`Star ${value}`}
-            onClick={() => handleStarClick(value)}
-            className={`${style.star} ${clickedStar === value ? style.clicked : ''}`}
+            onClick={(event) => handleStarClick(value,event)}
+            className={`${style.star} ${
+              clickedStar >= value ? style.clicked : ''
+            }`}
           />
         ))}
       </div>
