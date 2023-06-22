aa031b500a4a92d1d1eb1b23ed89dff93779b7a0
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-21 21:20:47 -0300

    Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 import style from './Navbar.module.css'
-// import searchIcon from '../../image/icons/search.png'
-// import { getVideogamesBySearch} from '../../redux/actions'
-// import { useDispatch } from 'react-redux'
 import SearchBarComponent from '../SearchBarComponent/SearchBarComponent'
+import logo from '../../image/logo.png'
 const Navbar = (props) => {
-
+  const {pathname} = useLocation()
   const {resetPagination} = props
 
   return (
     <nav>
       <div className={style['nav-container']}>
         <div className={style['nav-image']}>
-        <h1>Logo videogames</h1>
+          {/* <h1>Logo videogames</h1> */}
+        <img src={logo} alt='logo'/>
         </div>
 
         <SearchBarComponent resetPagination={resetPagination}/>
-        
         <div className={style['nav-routes']}>
-          <Link to={"/home"} style={{textDecoration:"none"}}><h3>Home</h3></Link>
-          <Link to="/create" style={{textDecoration:"none"}}><h3>Create a videogame!</h3></Link>
+          <Link to={"/home"} style={{textDecoration:"none"}}><h3 className={pathname === '/home' ? style["active"] : ""}>Home</h3></Link>
+          <Link to="/create" style={{textDecoration:"none"}}><h3 className={pathname === '/create' ? style["active"] : ""}>Create a videogame!</h3></Link>
           <Link to="/" style={{textDecoration:"none"}}><h3>Log out</h3></Link>
         </div>
       </div>
