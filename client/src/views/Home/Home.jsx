034c536810a45adf7ffea3fce0521f9644d35cd2
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-06-21 21:37:56 -0300

    Home

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -3,59 +3,48 @@ import Cards from '../../components/Cards/Cards'
 import Aside from '../../components/Aside/Aside'
 import style from './Home.module.css'
 import Pagination from '../../components/Pagination/Pagination'
-// import { useDispatch } from 'react-redux';
-// import { getAllVideogames } from '../../redux/actions';
-import { /*useDispatch*/ useSelector } from 'react-redux';
-// import { getAllVideogames } from '../../redux/actions';
+import { useSelector} from 'react-redux';
 
 const Home = (props) => {
 
-  const { currentPage, setCurrentPage } = props; //* Recibimos el state por props para el páginado
+  const { currentPage, setCurrentPage, resetPagination } = props; //* Recibimos el state por props para el páginado
 
-  const videogamesState = useSelector((state) => state.videogames);  //* Suscribimos el componente a redux con el hook useSelector
-  const videogamesBySearch = useSelector((state) => state.videoGamesBySearch);  //* Suscribimos el componente a redux con el hook useSelector
+  const videogames = useSelector((state) => state.videogames);  //* Suscribimos el componente a redux con el hook useSelector
 
   const ITEMS_PER_PAGE = 15; //* Establezco el límite de cartas por página
-  //* Creo un useState de los items = cards de videogames
-  const [items, setItems] = useState([]);
-
-  const videoGames = videogamesBySearch.length === 0 ? videogamesState : videogamesBySearch; //* videoGames tiene un valor condicional, es el videogamesState si videogamesBySearch no tiene longitud
   
+  const [items, setItems] = useState([]); //* Creo un useState de los items = cards de videogames
+
   //! Aquí establezco la cantidad de páginas según la cantidad de elementos/items/videogames 
-  const totalPages = Math.ceil(videoGames.length / ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(videogames.length / ITEMS_PER_PAGE);
   const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
 
   //* Cuando el componente se monta, hago que la página se establezca en 1 y dependerá de la función setCurrentPage y de videogamesBySearch
   useEffect(() => {
     setCurrentPage(1);
-  }, [setCurrentPage,videogamesBySearch]); //* Pongo como valor 1 a currentPage cuando videogamesBySearch cambie
+  }, [setCurrentPage]);
 
 
   //* en este useEffect, 
   useEffect(() => {
     const firstIndex = (currentPage - 1) * ITEMS_PER_PAGE;
     const lastIndex = firstIndex + ITEMS_PER_PAGE;
-    const slicedItems = videoGames.slice(firstIndex, lastIndex);
+    // const slicedItems = videoGames.slice(firstIndex, lastIndex);
+    const slicedItems = videogames.slice(firstIndex, lastIndex);
     setItems(slicedItems);
-  }, [currentPage, ITEMS_PER_PAGE, videoGames]);
+  }, [currentPage, ITEMS_PER_PAGE, videogames]);
 
   return (
     <div className={style['home-container']}>
-
       <div className={style['home']}>
-
-        <Aside videogames={items}/>
-
+        <Aside videogames={items} resetPagination={resetPagination}/>
         <div>
           <Cards videogames={items}/>
-          <div className={style['home-button']}>
-            <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} pageNumbers={pageNumbers}/>
-          </div>
-
+          <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} pageNumbers={pageNumbers}/>
         </div>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
