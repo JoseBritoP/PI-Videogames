5c4dab553de5399f3ebb9755a72dcb7510c5ec85
JoseBritoP <115720913+JoseBritoP@users.noreply.github.com>
2023-07-03 00:38:13 -0300

    Landing responsive

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link,useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import style from './Landing.module.css'
 import { useDispatch } from 'react-redux';
 import { getAllGenres, getAllVideogames,getAllPlatforms } from '../../redux/actions';
@@ -16,14 +16,15 @@ const Landing = () => {
   };
   return (
     <div className={style['landing-container']}>
-      <div className={style["landing-p"]}>
-        <p>Welcome to our gaming paradise! Explore a world of epic adventures, immersive gameplay, and endless fun. </p>
-        <p>Discover a vast collection of games, search by genre and rating, and find the perfect match for your gaming preferences. </p>
-        <p>Dive into detailed game profiles, unleash your creativity as a game creator, and connect with fellow gamers in our vibrant community. Level up your gaming adventure today!</p>
+      <div className={style['landing-back']}>
+        <div className={style["landing-p"]}>
+          <p>Welcome to our gaming paradise!</p>
+          <p> Explore a world of epic adventures, immersive gameplay, and endless fun. </p>
+          <p>Discover a vast collection of games, search by genre and rating, and find the perfect match for your gaming preferences. </p>
+          <p>Dive into detailed game profiles, unleash your creativity as a game creator, and connect with fellow gamers in our vibrant community. Level up your gaming adventure today!</p>
+          <input type="button" className={style['button']} value="Enjoy!" onClick={(e)=>handleStart(e)}/>
+        </div>
       </div>
-      <Link>
-        <input type="button" className={style['button']} value="Enjoy!" onClick={(e)=>handleStart(e)}/>
-      </Link>
     </div>
   )
 }
